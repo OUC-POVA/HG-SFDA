6da17c8cb850f08f8964da9ea6c5c7793907b17e
jiang jinkun <67258266+jiangkk-cloud@users.noreply.github.com>
2025-11-07 11:01:19 +0800

    Delete basicda directory

--- basicda/basicda/loaders/__init__.py
+++
@@ -1,4 +0,0 @@
-#
-# ----------------------------------------------
-
-from .builder import DATASETS, DATABUILDERS, parse_args_for_multiple_datasets, DefaultDataBuilder
--- basicda/basicda/models/schedulers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .builder import SCHEDULER, build_scheduler
-from .schedulers import *
-
-__all__ = [
-    'SCHEDULER', 'build_scheduler', 'TORCH_SCHEDULER', 'ConstantLR', 'PolynomialLR', 'WarmUpLR', 'InvLR'
-]
